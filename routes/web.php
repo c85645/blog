e40f97dec7d4e4aa6a8a69b789b944e2095858f2
Yuan <c85645@gmail.com>
2017-06-01 21:23:36 +0800

    First Commit!

--- routes/web.php
+++ routes/web.php
@@ -12,30 +12,45 @@
 */
 
 Route::get('/', function () {
+
+	// $me = new stdClass();
+	// $me = 'age' => 16;
+	// $me = 'gender' => 'male';
+
+	// echo dd($me);
+
+
+	$me2 = [
+		'age' => 16,
+		'gender' => 'male'
+	];
+
+	return dd($me2);
+
     //return view('welcome');
     // ArrayList
-    $members = [
-		'Stream' => [
-			'age' => 20,
-			'gender' => 'male'
-		],
-		'Sid' => [
-			'age' => 22,
-			'gender' => 'male'
-		],
-		'Jessie' => [
-			'age' => 18,
-			'gender' => 'female'
-		],
-		'Jeff' => [
-			'age' => 24,
-			'gender' => 'male'
-		],
-		'Eli' => [
-			'age' => 26,
-			'gender' => 'male'
-		]
-    ];
+	//   $members = [
+			// 'Stream' => [
+			// 	'age' => 20,
+			// 	'gender' => 'male'
+			// ],
+			// 'Sid' => [
+			// 	'age' => 22,
+			// 	'gender' => 'male'
+			// ],
+			// 'Jessie' => [
+			// 	'age' => 18,
+			// 	'gender' => 'female'
+			// ],
+			// 'Jeff' => [
+			// 	'age' => 24,
+			// 	'gender' => 'male'
+			// ],
+			// 'Eli' => [
+			// 	'age' => 26,
+			// 	'gender' => 'male'
+			// ]
+	//   ];
 
 	// for($i = 0 ; $i < count($members) ; $i++){
 	// 　echo $members[$i];
@@ -44,12 +59,56 @@ Route::get('/', function () {
 
     // foreach 的用法 （前面放陣列，後面放陣列內的物件名）
     // $member . '<br>'   當中的 . 就是連接的意思
-	foreach ($members as $member_name => $member_data) {
-		echo $member_name.', 性別：' . $member_data['gender'] . ', 年齡：' . $member_data['age'] . '<br>';
-	}
+	// foreach ($members as $member_name => $member_data) {
+	// 	echo $member_name.', 性別：' . $member_data['gender'] . ', 年齡：' . $member_data['age'] . '<br>';
+	// }
 });
 
-Route::get('/about', function(){
-	echo "1";
-	echo "2";
-}); 
+
+function checkRemainderOfNumber($number){
+	$remainder = $number % 2;
+
+	if ($remainder == 0){
+		return $number . '是偶數';
+	} else{
+		return $number . '是奇數';
+	}
+	return;
+}
+
+function sumOfNumbers($arr_numbers){
+	$start_value = 0;
+
+	foreach ($arr_numbers as $number){
+		$start_value += $number;
+	}
+	return $start_value;
+} 
+
+Route::get('/function_practice',function(){
+	$number = [1, 2, 3, 4, 5];
+
+	echo sumOfNumbers($number);
+
+	//有2個偶數
+});
+
+// function getRemainder($num1,$num2){
+// 	$remainder = $num1 % $num2;
+// 	return '餘數是. $remainder;
+// };
+
+// Route::get('/function_homework',function(){
+// 	echo countOfDay();
+// });
+
+// function countOfDay(){
+// 	$days = 0;
+
+// 	for($i = 1000; $i > 50; $i /= 2){
+// 		$days += 1;
+// 	}
+// 	echo $days;
+// }
+
+
