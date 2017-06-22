c3eb6819c9a86a65e7d66bcbfcd2cee0155349b9
Yuan <c85645@gmail.com>
2017-06-23 00:13:53 +0800

    修改初始化的練習頁面

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,11 +1,16 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<title></title>
-</head>
-<body>
-<h1>About us</h1>
-</body>
+	<head>
+		<title></title>
+	</head>
+	<body>
+		<h1>About us</h1>
+		<ui>
+			@foreach ($tasks as $task)
+				<li>{{ $task }}</li>
+			@endforeach
+		</ui>
+	</body>
 </html>
 
 
---
+++ resources/views/index.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Blog List</title>
+</head>
+
+<body>
+    <div id="page-wrapper" style="padding-top:51px;">
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header">最新資訊列表</h1>
+                <div class="row">
+                    <div class="container-fluid">
+                        <div style="margin-bottom:10px;" class="row">
+                            <ol class="breadcrumb">
+                                <li>最新資訊列表</li>
+                            </ol>
+                            <div class="btn-toolbar"><a href="" class="btn btn-primary">新增</a>
+                        </div>
+                        <table class="table table-striped table-middle table-bordered table-hover">
+                            <thead>
+                                <tr>
+                                    <th>標題</th>
+                                    <th>上架時間</th>
+                                    <th>下架時間</th>
+                                    <th>操作</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td class="del-name"><a href="">測試第一筆資料</a></td>
+                                    <td>2017-05-23 10:24:10</td>
+                                    <td>2017-06-03 16:57:08</td>
+                                    <td>
+                                        <form action="" method="POST" class="form-inline del-form"><a href="" class="btn btn-primary">修改</a>
+                                            <input type="hidden" name="_method" value="DELETE">
+                                            <input type="submit" value="刪除" class="btn btn-danger">
+                                        </form>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
