6c75b3f7be829d64e1b4bd4532d22c714f1a7c96
Yuan <c85645@gmail.com>
2017-06-29 20:28:25 +0800

    index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -17,35 +17,35 @@
                                 <li>最新資訊列表</li>
                             </ol>
                             <div class="btn-toolbar"><a href="" class="btn btn-primary">新增</a>
+                            </div>
+                            <table class="table table-striped table-middle table-bordered table-hover">
+                                <thead>
+                                    <tr>
+                                        <th>標題</th>
+                                        <th>上架時間</th>
+                                        <th>下架時間</th>
+                                        <th>操作</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td class="del-name"><a href="">測試第一筆資料</a></td>
+                                        <td>2017-05-23 10:24:10</td>
+                                        <td>2017-06-03 16:57:08</td>
+                                        <td>
+                                            <form action="" method="POST" class="form-inline del-form"><a href="" class="btn btn-primary">修改</a>
+                                                <input type="hidden" name="_method" value="DELETE">
+                                                <input type="submit" value="刪除" class="btn btn-danger">
+                                            </form>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
                         </div>
-                        <table class="table table-striped table-middle table-bordered table-hover">
-                            <thead>
-                                <tr>
-                                    <th>標題</th>
-                                    <th>上架時間</th>
-                                    <th>下架時間</th>
-                                    <th>操作</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td class="del-name"><a href="">測試第一筆資料</a></td>
-                                    <td>2017-05-23 10:24:10</td>
-                                    <td>2017-06-03 16:57:08</td>
-                                    <td>
-                                        <form action="" method="POST" class="form-inline del-form"><a href="" class="btn btn-primary">修改</a>
-                                            <input type="hidden" name="_method" value="DELETE">
-                                            <input type="submit" value="刪除" class="btn btn-danger">
-                                        </form>
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
 </body>
 
 </html>
