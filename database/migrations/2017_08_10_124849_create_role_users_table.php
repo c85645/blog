0d2e7f0981bacaf4d9129f161689836a9cd939a4
Yuan <c85645@gmail.com>
2017-08-10 21:29:16 +0800

    建立role,role_users table

--- database/migrations/2017_08_10_124849_create_role_users_table.php
+++ database/migrations/2017_08_10_124849_create_role_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateRoleUsersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,9 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->string('name');
+        Schema::create('role_users', function (Blueprint $table) {
+            $table->string('roleId')->unique();
             $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -29,6 +27,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('role_users');
     }
 }
