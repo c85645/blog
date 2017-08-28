5ac36adeff67e8ee5f8f1d0d024e7430239cd4b0
Yuan <c85645@gmail.com>
2017-08-28 09:09:40 +0800

    補上row class

--- resources/views/article/index.blade.php
+++ resources/views/article/index.blade.php
@@ -1,11 +1,35 @@
 @extends('layout')
 
 @section('html')
+
+{{-- table-middle的class ">" 連接下一層 直到td 給置中的css  --}}
+<style type="text/css">
+  .table-middle > tbody > tr > td{
+    vertical-align: middle;
+  }
+  .table-middle > tbody > tr > td > form{
+    vertical-align: middle;
+  }
+</style>
+
 <div class="container">
   <h1 class="page-header">文章列表</h1>
-  <a class="btn btn-primary" href="/article/create" role="button"><i class="fa fa-plus"> </i>新增</a>
-  <a class="btn btn-default" href="/" role="button"><i class="fa fa-home"></i>回上頁</a>
-  <table class="table table-bordered table-hover">
+  <div class="row">
+    <div class="col-xs-4">
+      <a class="btn btn-primary" href="/article/create" role="button"><i class="fa fa-plus"> </i>新增</a>
+      <a class="btn btn-default" href="/" role="button"><i class="fa fa-home"></i>回上頁</a>      
+    </div>
+    <div class="col-xs-offset-4 col-xs-4">
+      <div class="input-group">
+        <input type="text" class="form-control" placeholder="Search for...">
+        <span class="input-group-btn">
+          <button class="btn btn-default" type="button">Go!</button>
+        </span>
+      </div>
+    </div>
+  </div>
+
+  <table class="table table-bordered table-hover table-middle">
     <thead>
       <th width="20%">編號</th>
       <th width="30%">標題</th>
@@ -19,7 +43,7 @@
           <a href="/article/{{ $article->id }}">{{ $article->title }}</a>
         </td>
         <td>
-          <form class="form-inline" method="post" action="/article/{{ $article->id }}">
+          <form method="post" action="/article/{{ $article->id }}">
             <a class="btn btn-success" href="/article/{{ $article->id }}/edit"><i class="fa fa-pencil"></i>修改</a>
             <input type="hidden" name="_method" value="delete">
             <input type="hidden" name="_token" value="{{ csrf_token() }}">
